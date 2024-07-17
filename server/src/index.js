396d7f8cd25d470419989b6e8f3a97032a57d7d0
Ankit Shrestha <ankitshrestha008@gmail.com>
2024-07-17 16:57:57 +0545

    Mongodb batabase connected

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 const port = 8000
 const userRoute = require('./routes/users')
+const connect = require('./db/connection')
 app.use(userRoute)
+connect()
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
