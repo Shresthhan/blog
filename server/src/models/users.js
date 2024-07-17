6c8f24830bee3fedd640c23d51bb92910ddaab29
Ankit Shrestha <ankitshrestha008@gmail.com>
2024-07-17 17:06:49 +0545

    UserSchema completed

--- server/src/models/users.js
+++ server/src/models/users.js
@@ -3,15 +3,11 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
   title: String, // String is shorthand for {type: String}
-//   author: String,
-//   body: String,
-//   comments: [{ body: String, date: Date }],
-//   date: { type: Date, default: Date.now },
-//   hidden: Boolean,
-//   meta: {
-//     votes: Number,
-//     favs: Number
-//    }
+  author: String,
+  body: String,
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now },
+  hidden: Boolean,
 });
 const User = mongoose.model('Blog', userSchema);
 module.exports = User
